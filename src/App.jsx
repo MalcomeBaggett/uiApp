454206476ac0164278bf99ffca350856a038b4a2
Malcome Baggett <malcomebagggett@gmail.com>
2023-06-21 16:30:32 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import Button from "./Button"
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div>
+        <Button>Hello</Button>
+      </div>
+      <div>
+        <Button>Hello</Button>
+      </div>
+      <div>
+        <Button>Hello</Button>
+      </div>
+      <div>
+        <Button>Hello</Button>
+      </div>
+      <div>
+        <Button>Hello</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Button.jsx
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types"
+const Button = ({
+  children,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+}) => {
+  return (
+    <button className="px-3 py-1.5 border border-blue-600 bg-blue-500 text-white">
+      {children}
+    </button>
+  )
+}
+
+Button.propTypes = {
+  checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!warning) +
+      Number(!!success) +
+      Number(!!danger)
+
+    if (count > 1) {
+      return new Error(
+        "Failed prop type: only one type of button prop can be passed down"
+      )
+    }
+  },
+}
+
+export default Button
