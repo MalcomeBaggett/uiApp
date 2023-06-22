67694f42a039e4b2406866df424ce605987f75b2
Malcome Baggett <malcomebagggett@gmail.com>
2023-06-22 16:32:22 -0400

    changed folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,10 @@
 import { useState } from "react"
 import { GoBell, GoDownload, GoDatabase } from "react-icons/go"
-import Button from "./Button"
+import ButtonPage from "./pages/ButtonPage"
 function App() {
-  const handleClick = () => {
-    console.log("click")
-  }
   return (
     <>
-      <div>
-        <Button primary onClick={handleClick}>
-          <GoBell />
-          Hello
-        </Button>
-      </div>
-      <div>
-        <Button secondary>
-          <GoDownload />
-          Hello
-        </Button>
-      </div>
-      <div>
-        <Button success>
-          <GoDatabase />
-          Hello
-        </Button>
-      </div>
-      <div>
-        <Button warning>Hello</Button>
-      </div>
-      <div>
-        <Button danger>Hello</Button>
-      </div>
+      <ButtonPage />
     </>
   )
 }
---
+++ src/pages/ButtonPage.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import { GoBell, GoDownload, GoDatabase } from "react-icons/go"
+import Button from "../components/Button"
+
+function ButtonPage() {
+  const handleClick = () => {
+    console.log("click")
+  }
+  return (
+    <>
+      <div>
+        <Button primary onClick={handleClick}>
+          <GoBell />
+          jared rocks
+        </Button>
+      </div>
+      <div>
+        <Button secondary>
+          <GoDownload />
+          Hello
+        </Button>
+      </div>
+      <div>
+        <Button success>
+          <GoDatabase />
+          Hello
+        </Button>
+      </div>
+      <div>
+        <Button warning>Hello</Button>
+      </div>
+      <div>
+        <Button danger>Hello</Button>
+      </div>
+    </>
+  )
+}
+
+export default ButtonPage
